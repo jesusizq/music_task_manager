d4230b65f2c521ba0776c0e022661f22cbbeb0d2
Jesús Izquierdo <susej_ic@hotmail.com>
2024-08-20 16:40:24 +0200

    Improve data processor endpoints

--- app/api/data_processor.py
+++ app/api/data_processor.py
@@ -1,5 +1,6 @@
 import os
-from flask import request, jsonify
+from flask import request, jsonify, send_file
+from app import db
 from . import data_processor_api as api
 from rabbit import rabbitmq_client
 from app.models import Task, TaskStatus, TaskType
@@ -15,7 +16,6 @@ rabbit_client = rabbitmq_client.RabbitMQClient(
 
 LOCAL_STORAGE_PATH = os.environ.get("LOCAL_STORAGE_PATH", "../../../local_storage")
 raw_storage = os.path.join(LOCAL_STORAGE_PATH, "raw_data")
-processed_storage = os.path.join(LOCAL_STORAGE_PATH, "processed_data")
 
 
 @api.route("/health", methods=["GET"])
@@ -35,18 +35,20 @@ def upload_csv():
     task_id = str(uuid.uuid4())
     filename = f"raw_{task_id}.csv"
 
-    # Save file (simulate processing time)
+    # Save file
     file_path = os.path.join(raw_storage, filename)
     file.save(file_path)
-    time.sleep(5)
+    time.sleep(5)  # Simulate processing time
 
     # Create a new task and save it in database
     task = Task(
         task_type=TaskType.CSV, status=TaskStatus.PROCESSING, file_input_path=file_path
     )
+    db.session.add(task)
+    db.session.commit()
 
     # Publish message
-    message = {"task_id": task_id, "file_content": file.read().decode("utf-8")}
+    message = {"task_id": task_id, "file_path": file_path}
     rabbit_client.publish(message)
 
     return jsonify({"task_id": task_id}), 202
@@ -54,18 +56,38 @@ def upload_csv():
 
 @api.route("/status/<task_id>", methods=["GET"])
 def get_status(task_id):
-    # TODO: Implement download logic
-    return (
-        jsonify(
-            {
-                "download_link": f"http://localhost:5000/v1/data_processor/download/{task_id}"
-            }
-        ),
-        200,
-    )
+    task = Task.query.get(task_id)
+    if not task:
+        return jsonify({"error": "Task not found"}), 404
+
+    if task.status == TaskStatus.DONE:
+        return (
+            jsonify(
+                {
+                    "status": task.status.name,
+                    "download_link": f"http://localhost:5000/v1/data_processor/download/{task_id}",
+                }
+            ),
+            200,
+        )
+
+    return jsonify({"status": task.status.name}), 200
 
 
 @api.route("/download/<task_id>", methods=["GET"])
 def download_csv(task_id):
-    # TODO: Implement download logic
-    return jsonify({"message": "File ready for download"}), 200
+    task = Task.query.get(task_id)
+    if not task:
+        return jsonify({"error": "Task not found"}), 404
+
+    if task.status == TaskStatus.PROCESSING or task.status == TaskStatus.RECEIVED:
+        return jsonify({"error": "Task processing"}), 400
+    elif task.status == TaskStatus.FAILED:
+        return jsonify({"error": "Task failed"}), 400
+    elif task.status == TaskStatus.CANCELLED:
+        return jsonify({"error": "Task cancelled"}), 400
+    elif task.status == TaskStatus.DONE:
+        time.sleep(5)  # Simulate download time
+        return send_file(task.file_output_path, as_attachment=True), 200
+    else:
+        return jsonify({"error": "Unknown status"}), 400
